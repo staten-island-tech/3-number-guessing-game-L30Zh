5376827840915885450c82a45f70b1b0857e3153
L30Zh <LEoz47@nycstudents.net>
2025-09-26 09:42:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ while True:
    
     y = int(y)
     x = input ("Okay, pick a number 1-10")
-    while x != y:
-        input ("Wrong! Try again!")
+    while x == y:
+        input ("YEAHHAHHAHAHAHHH")
     if int(x) > y:
         input("Too high!")
     elif int(x) < y:
-        print("Too low!")
+        input("Too low!")
     elif int(x) == y:
         print ("YEAHHAHHAHAHAHHH")
-        break
+    
+    
 
